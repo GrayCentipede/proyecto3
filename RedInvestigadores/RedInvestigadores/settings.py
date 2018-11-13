f86f3efcfdb9efa10de53f24292efa0a362ae214
GrayCentipede <maucarrui@gmail.com>
2018-11-13 00:44:43 -0600

    Added django-seed dependency

--- RedInvestigadores/RedInvestigadores/settings.py
+++ RedInvestigadores/RedInvestigadores/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_seed',
 ]
 
 ROOT_URLCONF = 'RedInvestigadores.urls'
