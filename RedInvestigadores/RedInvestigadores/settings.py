58165413d6da5a7850974ecad71b45c0591444a9
GrayCentipede <maucarrui@gmail.com>
2018-11-13 00:50:03 -0600

    Fixed django-seed location

--- RedInvestigadores/RedInvestigadores/settings.py
+++ RedInvestigadores/RedInvestigadores/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_seed',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_seed',
 ]
 
 ROOT_URLCONF = 'RedInvestigadores.urls'
